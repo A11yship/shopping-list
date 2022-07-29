8aaf932ff292b0652f4c579141433d5a338d7ec3
Hannah Steil <h-steil@t-online.de>
2022-07-29 15:23:17 +0200

    Add persist to save in local Storage

--- src/useStore.js
+++ src/useStore.js
@@ -1,32 +1,29 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
-const useStore = create((set) => {
-  return {
-    shoppingItems: [
-      {
-        _id: "c2hvcHBpbmcuaXRlbTox-test",
-        _type: "shopping.item",
-        category: { _type: "ref", _ref: "c2hvcHBpbmcuY2F0ZWdvcnk6MA==" },
-        name: { en: "Pineapple-test", de: "Ananas-test" },
+const useStore = create(
+  persist(
+    (set) => ({
+      shoppingItems: [],
+      addItem: (newItem) => {
+        set((state) => {
+          return {
+            shoppingItems: [...state.shoppingItems, newItem],
+          };
+        });
       },
-    ],
-    addItem: (newItem) => {
-      set((state) => {
-        return {
-          shoppingItems: [...state.shoppingItems, newItem],
-        };
-      });
-    },
-    removeItem: (currentItemId) => {
-      set((state) => {
-        return {
-          shoppingItems: state.shoppingItems.filter(
-            (shoppingItem) => shoppingItem._id !== currentItemId
-          ),
-        };
-      });
-    },
-  };
-});
+      removeItem: (currentItemId) => {
+        set((state) => {
+          return {
+            shoppingItems: state.shoppingItems.filter(
+              (shoppingItem) => shoppingItem._id !== currentItemId
+            ),
+          };
+        });
+      },
+    }),
+    { name: "shoppingItems", getStorage: (shoppingItem) => localStorage }
+  )
+);
 
 export default useStore;
