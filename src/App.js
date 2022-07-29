6e5d9f3befc4f0cfb108f8011e9bf312cc8eb897
Hannah Steil <h-steil@t-online.de>
2022-07-29 11:06:58 +0200

    Add List styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import { items } from "./example-items";
 import ShoppingListItem from "./ShoppingListItem";
+import StyledList from "./StyledList";
 
 function App() {
   return (
     <div className="App">
       <h1> Einkaufsliste </h1>
-      <ul>
+      <StyledList>
         {" "}
         {items.map((item) => (
           <ShoppingListItem key={item._id} name={item.name.de} />
         ))}{" "}
-      </ul>
+      </StyledList>
       <label for="shoppingItem"> Was möchtest du einkaufen? </label>
       <input type={"text"} id="shoppingItem" name="shoppingItem"></input>
     </div>
---
+++ src/StyledList.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 5px;
+  flex-wrap: wrap;
+`;
+
+export default StyledList;
