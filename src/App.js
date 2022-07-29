67770dbe2a533263e9f1e41e7b2e856e5be7ea36
Hannah Steil <h-steil@t-online.de>
2022-07-29 11:54:32 +0200

    Add search function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { items } from "./example-items";
 import ShoppingListItem from "./ShoppingListItem";
 import StyledList from "./StyledList";
 import useStore from "./useStore";
+import { search } from "fast-fuzzy";
+import { useState } from "react";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
   const selectedItems = useStore((state) => state.shoppingItems);
   const addItem = useStore((state) => state.addItem);
 
@@ -17,8 +20,25 @@ function App() {
           <ShoppingListItem key={item._id} name={item.name.de} />
         ))}{" "}
       </StyledList>
-      <label for="shoppingItem"> Was möchtest du einkaufen? </label>
-      <input type={"text"} id="shoppingItem" name="shoppingItem"></input>
+      <label htmlFor="shoppingItem"> Was möchtest du einkaufen? </label>
+      <input
+        type={"text"}
+        id="shoppingItem"
+        name="shoppingItem"
+        onChange={(event) => {
+          setSearchResults(
+            search(event.target.value, items, {
+              keySelector: (obj) => obj.name.de,
+            })
+          );
+        }}
+      ></input>
+      <StyledList>
+        {" "}
+        {searchResults.map((item) => (
+          <ShoppingListItem key={item._id} name={item.name.de} />
+        ))}{" "}
+      </StyledList>
     </div>
   );
 }
